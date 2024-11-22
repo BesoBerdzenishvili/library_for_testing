2c77911f5d23b78fcf4a226969083836402bc7d8
fliderman01 <fliderman01@gmail.com>
2024-11-22 19:04:01 +0400

    add: Book

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { Table, Container } from "react-bootstrap";
 import InfiniteScroll from "react-infinite-scroll-component";
 import { fr, en, ru, Faker } from "@faker-js/faker";
+import Book from "./Book";
 
 const Books = ({ books }) => {
   const [data, setData] = useState([]);
@@ -24,8 +25,10 @@ const Books = ({ books }) => {
   useEffect(() => {
     faker.seed(Number(books.seed));
 
-    const initialData = generateData(books.likes, books.review, 20);
-    setData(initialData);
+    if (books.seed) {
+      const newData = generateData(books.likes, books.review, 20);
+      setData(newData);
+    }
   }, [books]);
 
   const fetchMoreData = () => {
@@ -38,54 +41,65 @@ const Books = ({ books }) => {
     for (let i = 0; i < count; i++) {
       data.push({
         id: i + 1,
-        cover: faker.image.urlPicsumPhotos(), //try others too https://fakerjs.dev/api/image.html
+        cover: faker.image.urlLoremFlickr({
+          height: 380,
+          width: 280,
+          category: "abstract",
+        }),
         isbn: faker.commerce.isbn(),
         title: faker.book.title(),
         author: faker.book.author(),
         publisher: faker.book.publisher(),
-        likes: Math.round(likes * 10) / 10,
-        review: Math.round(review * 10) / 10,
-        // how does this create controlled randomness?
+        likes: likes,
+        review: review,
       });
     }
     return data;
   };
 
   return (
-    <Container>
-      <InfiniteScroll
-        dataLength={data.length}
-        hasMore={true}
-        next={fetchMoreData}
-        loader={<div className="loader text-center">Loading...</div>}
-        endMessage={
-          <div className="end-message text-center">No more books to show</div>
-        }
-      >
-        <Table sticky="top">
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>ISBN</th>
-              <th>Title</th>
-              <th>Author(s)</th>
-              <th>Publisher</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.map((book, index) => (
-              <tr key={book.id + book.isbn}>
-                <td>{index + 1}</td>
-                <td>{book.isbn}</td>
-                <td>{book.title}</td>
-                <td>{book.author}</td>
-                <td>{book.publisher}</td>
-              </tr>
-            ))}
-          </tbody>
-        </Table>
-      </InfiniteScroll>
-    </Container>
+    <>
+      {books.seed ? (
+        <Container>
+          <InfiniteScroll
+            dataLength={data.length}
+            hasMore={true}
+            next={fetchMoreData}
+            loader={<div className="loader text-center">Loading...</div>}
+            endMessage={
+              <div className="end-message text-center">
+                No more books to show
+              </div>
+            }
+          >
+            <Table sticky="top">
+              <thead>
+                <tr>
+                  <th></th>
+                  <th>#</th>
+                  <th>ISBN</th>
+                  <th>Title</th>
+                  <th>Author(s)</th>
+                  <th>Publisher</th>
+                </tr>
+              </thead>
+              <tbody>
+                {data.map((book, index) => (
+                  <Book
+                    key={book.id + book.isbn}
+                    book={book}
+                    index={index}
+                    language={[chooseLanguage(), en]}
+                  />
+                ))}
+              </tbody>
+            </Table>
+          </InfiniteScroll>
+        </Container>
+      ) : (
+        <p className="text-center">Click "GENERATE" to generate Books</p>
+      )}
+    </>
   );
 };
 
